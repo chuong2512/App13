25ff76f84a5a3491c44b68e818550833bb773a65
chuong_gamer <hieutg244@gmail.com>
2023-07-09 01:12:06 +0700

    up

--- Assets/_App/Scripts/Shop/DonHang.cs
+++ Assets/_App/Scripts/Shop/DonHang.cs
@@ -6,26 +6,15 @@ public class DonHang : SanPham
 {
     public int donHangID;
 }
-/// <summary>
-/// /////
-/// </summary>
+
 [Serializable]
 public class SanPham
 {
     public int ID;
     public Sprite anh;
-    /// <summary>
-    /// ////////
-    /// </summary>
-    [HideInInspector]public Sprite anhHai;
-    [HideInInspector]public string thongTin;
-    /// <summary>
-    /// /////
-    /// </summary>
+    public Sprite anhHai;
+    public string thongTin;
     public string gia;
-    /// <summary>
-    /// /////
-    /// </summary>
     public string name;
 }
 
